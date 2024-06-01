659ee5e487cb830a19c1ee0b716625d85e7b1d49
brenner.souza <brenner.souza@gmail.com>
2024-06-01 00:23:06 -0300

    Projeto base

---
+++ src/main/java/brennerfsouza/frasesclassicas/FrasesclassicasApplication.java
@@ -0,0 +1,13 @@
+package brennerfsouza.frasesclassicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesclassicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesclassicasApplication.class, args);
+	}
+
+}
